6138f6aa08c091a72eb5c39a39cfad26ed4848ae
BeePhuc <156519463+BeePhuc@users.noreply.github.com>
2024-02-03 00:07:45 +0700

    Practice 7.sql

--- Day 15_Practice 7.sql
+++ Day 15_Practice 7.sql
@@ -51,4 +51,53 @@ ROUND((1.0*(tweet_count + a + b)/
 (CASE WHEN a=0 AND b=0 THEN 1
       WHEN b=0 THEN 2
 ELSE 3 END)), 2) AS rolling_avg_3d
-FROM cte
+FROM cte;
+---ex6
+---solution1
+WITH cte AS
+(SELECT merchant_id, transaction_timestamp,
+LAG(transaction_timestamp) OVER (PARTITION BY merchant_id,credit_card_id,amount
+ORDER BY transaction_timestamp) AS previous_transaction_timestamp
+FROM transactions),
+cte_1 AS
+(SELECT *,
+EXTRACT (EPOCH FROM (transaction_timestamp - previous_transaction_timestamp))/60 AS diff
+FROM cte)
+
+SELECT COUNT (*) FROM cte_1
+WHERE diff <= 10;
+---solution2
+WITH cte AS
+(SELECT *,
+LAG(transaction_timestamp) OVER (PARTITION BY merchant_id,credit_card_id,amount 
+ORDER BY transaction_timestamp) AS previous_transaction_timestamp
+FROM transactions)
+
+SELECT COUNT (*) FROM transactions a
+JOIN cte b ON a.transaction_id=b.transaction_id
+WHERE a.merchant_id=b.merchant_id AND
+      a.credit_card_id=b.credit_card_id AND
+      a.amount=b.amount AND
+      EXTRACT (EPOCH FROM (a.transaction_timestamp - previous_transaction_timestamp))/60 <= 10;
+---ex7
+SELECT category, product, total_spend
+FROM 
+(SELECT category, product, SUM (spend) AS total_spend,
+RANK () OVER (PARTITION BY category ORDER BY SUM(spend) DESC) AS ranking
+FROM product_spend
+WHERE EXTRACT (year FROM transaction_date) ='2022'
+GROUP BY category, product) AS a
+WHERE ranking <= 2;
+---ex8
+WITH top10 AS 
+(SELECT artist_name,
+DENSE_RANK() OVER (ORDER BY COUNT(b.song_id) DESC) AS artist_rank
+FROM artists a
+JOIN songs b ON a.artist_id = b.artist_id
+JOIN global_song_rank c ON b.song_id = c.song_id
+WHERE rank <= 10
+GROUP BY artist_name)
+
+SELECT artist_name, artist_rank
+FROM top10
+WHERE artist_rank <= 5;
