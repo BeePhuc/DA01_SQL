cfe841711c373581b1bec74c7d7b361d01d267aa
BeePhuc <156519463+BeePhuc@users.noreply.github.com>
2024-02-03 22:18:05 +0700

    Day 16_Practice 8.sql

--- Day 16_Practice 8.sql
+++ Day 16_Practice 8.sql
@@ -1,7 +1,7 @@
 ---ex1
 WITH a AS
-(SELECT MIN(order_date) AS first_date, customer_id FROM Delivery
-GROUP BY customer_id)
+(SELECT DISTINCT customer_id,
+FIRST_VALUE (order_date) OVER (PARTITION BY customer_id ORDER BY order_date) AS first_date FROM Delivery)
 
 SELECT ROUND(SUM(CASE WHEN b.customer_pref_delivery_date=first_date THEN 1 ELSE 0 END)
             /COUNT(DISTINCT b.customer_id)*100, 2) AS immediate_percentage
